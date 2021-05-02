9bf2fb652ec58bbf1ac41c3d08a1fc807a3e3608
Yaazfei <yaazif@gmail.com>
2021-05-02 01:03:17 -0300

    Adiciona arquivo War e Docker

--- src/main/java/br/com/yaazfei/seguradoradeveiculos/SeguradoraDeVeiculosApplication.java
+++ src/main/java/br/com/yaazfei/seguradoradeveiculos/SeguradoraDeVeiculosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SeguradoraDeVeiculosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeguradoraDeVeiculosApplication.class, args);
 	}
-
 }
